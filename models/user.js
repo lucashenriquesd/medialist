221b6e601f9d32ad2998b373d1f2030dba7dbda9
Lucas Henrique <lucashenriquesd@gmail.com>
2022-01-19 17:10:51 -0300

    removed password from model select

--- models/user.js
+++ models/user.js
@@ -21,6 +21,9 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: { exclude: ['password'] },
+    }
   });
   return User;
 };
