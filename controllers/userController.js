3e3a25f3137500a3fafb36751ad565686d5a491c
Lucas Henrique <lucashenriquesd@gmail.com>
2022-01-20 00:38:35 -0300

    added user not found

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,24 @@ const { User } = require('../models');
 
 exports.index = async function(req, res) {
     const users = await User.findAll();
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(users, null, 2));
+
+    if (users === null) {
+        await res.status(404).send({ "msg": `No User found` });
+        return;
+    }
+
+    res.send(users);
 };
 
 exports.show = async function(req, res) {
     const id = parseInt(req.params.id);
     const user = await User.findByPk(id);
-    res.setHeader('Content-Type', 'application/json');
+
+    if (user === null) {
+        await res.status(404).send({ "msg": `User ${id} not found` });
+        return;
+    }
+
     res.send(JSON.stringify(user, null, 2));
 };
 
@@ -17,6 +27,11 @@ exports.update = async function(req, res) {
     const id = parseInt(req.params.id);
     const { name, nickName } = req.body;
     const user = await User.findByPk(id);
+
+    if (user === null) {
+        await res.status(404).send({ "msg": `User ${id} not found` });
+        return;
+    }
     user.set({
         name: name,
         nickName: nickName
@@ -24,19 +39,24 @@ exports.update = async function(req, res) {
 
     await user.save();
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "msg": `User ${req.params.id} updated` }, null, 2));
+    res.send({ "msg": `User ${id} updated` });
 };
 
 exports.delete = async function(req, res) {
     const id = parseInt(req.params.id);
     const user = await User.findByPk(id);
 
+    if (user === null) {
+        await res.status(404).send({ "msg": `User ${id} not found` });
+        return;
+    }
+
     await user.destroy();
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "msg": `User ${req.params.id} deleted` }, null, 2));
+    res.send({ "msg": `User ${id} deleted` });
 };
 
 exports.store = async function(req, res) {
     const user = await User.create(req.body);
-    res.send(`User ${req.body.name} created`);
+    res.send({ "msg": `User ${req.body.name} created` });
 };
